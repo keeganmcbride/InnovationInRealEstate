25c8d425ab27a14eb80c2091fd877dbe63bb0ca9
Keegan David Braun Mcbride <kemcbr@ttu.ee>
2017-11-30 11:03:56 +0200

    First Files

---
+++ Application/app.R
@@ -0,0 +1,20 @@
+#Initialize packages
+library(shiny); library(leaflet); library(ggmap);
+
+#User Interface
+ui <- navbarPage(
+  
+)
+
+
+
+
+#Server
+server <- function(input, output) {
+  
+  
+}
+
+
+# Run Application 
+shinyApp(ui = ui, server = server)
